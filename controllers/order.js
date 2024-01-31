dd346944552411a255d9a817fcb3139a6557879b
mirili1 <150081460+mirili1@users.noreply.github.com>
2024-01-31 16:06:03 +0200

    Update order.js

--- controllers/order.js
+++ controllers/order.js
@@ -74,7 +74,7 @@ export const sendingOrder=async(req,res)=>{
         if(!order)
             return res.status(404).send("there are no such an order")
         if(order.isSent)
-            return res.status(400).send("the order was sent already");
+            return res.status(200).send("the order was sent already");
         order.isSent=true;
         await order.save();
         res.status(200).send("sent successfully")    
