9f1610f778d7726a0b8164cdd95b0e9f75125ab0
mirili1 <150081460+mirili1@users.noreply.github.com>
2024-02-07 17:54:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import shoppingCartRouter from './routes/ShoppingCart.js'
 config();
 connectToDB()
 const app=express();
-app.use(cors({ origin: 'https://shopcore.onrender.com' }));
+app.use(cors());
 app.use(morgan("common"));
 app.use(express.json());
 app.use(express.static('images'));
